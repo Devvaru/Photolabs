1c9808cdc323acf0da0bc38500a748ab25d5d1e4
Devvaru <syeraru@gmail.com>
2023-09-15 19:24:04 -0600

    Move nav and photo section to HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,15 @@
 import React, { useCallback, useState } from 'react';
 
-// import PhotoListItem from './components/PhotoListItem';
-import PhotoList from './components/PhotoList';
-import TopNavigationBar from './components/TopNavigationBar';
+import HomeRoute from 'routes/HomeRoute';
 
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
-  // const photos = new Array(3).fill(null);
-
   return (
     <div className="App">
-
-      {/* {photos.map((_, index) => (
-        <PhotoListItem
-          key={index}
-          id={sampleDataForPhotoListItem.id}
-          location={sampleDataForPhotoListItem.location}
-          imageSource={sampleDataForPhotoListItem.imageSource}
-          username={sampleDataForPhotoListItem.username}
-          profile={sampleDataForPhotoListItem.profile}
-        />
-      ))} */}
-      <TopNavigationBar />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
