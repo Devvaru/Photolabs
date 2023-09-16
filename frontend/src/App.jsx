9cbd9161d7d5fed1b4703cb25a6a6a7fdbb030f3
Devvaru <syeraru@gmail.com>
2023-09-15 19:18:35 -0600

    Nest nav bar components to display nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import React, { useCallback, useState } from 'react';
 
 // import PhotoListItem from './components/PhotoListItem';
 import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
+import TopNavigationBar from './components/TopNavigationBar';
+
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -23,7 +24,7 @@ const App = () => {
           profile={sampleDataForPhotoListItem.profile}
         />
       ))} */}
-      <TopicList />
+      <TopNavigationBar />
       <PhotoList />
     </div>
   );
