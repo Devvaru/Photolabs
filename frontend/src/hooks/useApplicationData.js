db9df0dbc02adeeb0ebc0771b59ce0c513bbd142
Devvaru <syeraru@gmail.com>
2023-09-21 23:58:33 -0600

    Refator to use useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,38 +1,95 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+
+  CLOSE_PHOTO_MODAL: 'CLOSE_PHOTO_MODAL',
+
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  
+  // SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+
+  // SELECT_PHOTO: 'SELECT_PHOTO',
+
+  // DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+}
 
 const useApplicationData = () => {
 
-  const [state, setState] = useState({
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case ACTIONS.FAV_PHOTO_ADDED:
+        const photoIdToAdd = action.payload;
+        if (!state.favouritePhotoIds.includes(photoIdToAdd)) {
+          const copyOfFavouritePhotoIds = [...state.favouritePhotoIds, photoIdToAdd];
+          return { ...state, favouritePhotoIds: copyOfFavouritePhotoIds };
+        }
+
+      case ACTIONS.FAV_PHOTO_REMOVED:
+        const photoIdToRemove = action.payload;
+        if (state.favouritePhotoIds.includes(photoIdToRemove)) {
+          const copyOfFavouritePhotoIds = state.favouritePhotoIds.filter(
+            (favPhotoID) => favPhotoID !== photoIdToRemove
+          );
+          return { ...state, favouritePhotoIds: copyOfFavouritePhotoIds };
+        }
+
+      //   case ACTIONS.FAV_PHOTO_ADDED:
+      //   return {
+      //     ...state,
+      //     favouritePhotoIds: [...state.favouritePhotoIds, action.payload],
+      //   };
+
+      // case ACTIONS.FAV_PHOTO_REMOVED:
+      //   return {
+      //     ...state,
+      //     favouritePhotoIds: state.favouritePhotoIds.filter(
+      //       (favPhotoID) => favPhotoID !== action.payload
+      //     ),
+      //   };
+
+      case ACTIONS.SET_PHOTO_DATA:
+        const { data, isOpen } = action.payload;
+        return { ...state, photoData: { ...data }, displayModal: isOpen };
+
+      case ACTIONS.CLOSE_PHOTO_MODAL:
+        return { ...state, displayModal: false };
+
+      default:
+        return state;
+    }
+  }
+
+  const initialState = {
     favouritePhotoIds: [],
     displayModal: false,
     photoData: {},
-  });
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   // set favourite photos array
   const updateFavouritePhotoIds = (photoID) => {
-
     if (state.favouritePhotoIds.includes(photoID)) {
-
-      // Remove photo ID if already in favourites
-      const copyOfFavourites = state.favouritePhotoIds.filter((favPhotoID) => photoID !== favPhotoID)
-      setState({...state, favouritePhotoIds: copyOfFavourites})
-
+      // ID exists, dispatch a removal action
+      dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, payload: photoID });
     } else {
-
-      // Add photo ID if not in favourites
-      const modifiedFavourites = [...state.favouritePhotoIds, photoID]
-      setState({...state, favouritePhotoIds: modifiedFavourites})
+      // ID doesn't exist, dispatch an addition action
+      dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: photoID });
     }
   };
 
+  console.log(state.favouritePhotoIds)
+
   // sets photo data for modal and sets displayModal boolean
   const setPhotoData = (data, isOpen = false) => {
-    setState({ ...state, photoData: { ...data }, displayModal: isOpen })
+    dispatch({ type: ACTIONS.SET_PHOTO_DATA, payload: { data, isOpen } });
   };
 
   // closes modal
   const onClosePhotoDetailsModal = () => {
-    setState({ ...state, displayModal: false })
+    dispatch({ type: ACTIONS.CLOSE_PHOTO_MODAL })
   };
 
   // const onOpenPhotoDetailsModal = () => {
